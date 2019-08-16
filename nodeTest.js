6b715f636e575970f47e48984f144ab919eb0699
ThembiShabalala <thembi.shabalala@umuzi.org>
2019-08-16 16:12:31 +0200

    second draft

---
+++ nodeTest.js
@@ -0,0 +1,52 @@
+'use strict';
+const fs = require('fs');
+
+
+module.exports = class Visitor{
+    constructor(fullName, age, dateOfVisit, timeOfVisit, comments, nameOfAssistance){
+        this.name = fullName;
+        this.age = age;
+        this.date = dateOfVisit;
+        this.time = timeOfVisit;
+        this.comments = comments;
+        this.nameOfAssistance = nameOfAssistance;
+        this.id;
+        
+
+    }
+
+    save(id) {
+        let newUser = {
+        id: this.id,
+        fullName: this.name,
+        age : this.age,
+        date : this.date,
+        time : this.time,
+        comments : this.comments,
+        nameOfAssistance : this.nameOfAssistance,
+        }
+        let data = JSON.stringify(newUser);
+        console.log(data);
+
+        fs.writeFileSync(`visitor${id}.json`, data);
+
+        console.log(data);
+        
+        // let visitor = JSON.parse(data); //passing data of visitors from json
+        return; //visitor;
+        
+    }
+
+    load(id) {
+        const fs = require("fs")
+        if (id <= 0) {
+             console.log("File doesn't exist");
+        }
+        var fileID = `visitor${id}.json`;
+        var visitor = fs.readFileSync(fileID);
+        let visitorData = JSON.parse(visitor);
+        return visitorData;
+   }
+}
+
+
---
+++ nodeTestSpec.js
@@ -0,0 +1,48 @@
+describe('Visitor()', () => {
+    let visitor;
+    beforeEach(() => {
+        let visitor = require("./nodeTest");
+        visitor = new Visitor("Joseph", 37, "10/10/10", "13h00", "visiting a sister", "Melissa")
+    })
+    it('should be defined', () => {
+        expect(Visitor).toBeDefined();
+    });
+})
+​describe('Visitor.save()', () => {
+    let visitor;
+    beforeEach(() => {
+        visitor = new Visitor("Joseph", 37, "10/10/10", "13h00", "visiting a sister", "Melissa")
+    })
+    it('save() should be defined', () => {
+        expect(visitor.save()).toBeDefined()
+    })
+    it('save() should create a new .json file', () => {
+        visitor = new Visitor("Joseph", 37, "10/10/10", "13h00", "visiting a sister", "Melissa")
+        expect(visitor.save()).toBe('File was saved');
+    });
+    it('should throw an error when the function is unable to create a new file ', () => {
+        visitor = function () {
+            throw ('File cannot be saved');
+        }
+        expect(visitor).toThrow('File cannot be saved');
+    });
+})
+​describe('Visitor.load()', () => {
+    let visitor;
+    beforeEach(() => {
+        visitor = new Visitor("Joseph", 37, "10/10/10", "13h00", "visiting a sister", "Melissa")
+    })
+    it('load() should be defined', () => {
+        expect(visitor.load(1)).toBeDefined();
+    });
+    it('should return the file with the defined ID', () => {
+        expect(visitor.load(1)).toEqual({
+            fullName: "Joseph",
+            age: 37,
+            dateOfVisit: "10/10/10",
+            timeOfVisit: "13h00",
+            comments: "visiting a sister",
+            assistantName: "Melissa",
+        });
+    })
+})
